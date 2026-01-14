32904c20df7e6df655166df24227b5d5241b72c1
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2026-01-14 19:34:50 +0300

    added not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,15 @@
+import image from '@images/404.svg';
+import { Typography, Button, Flex } from 'antd';
+import { useNavigate } from 'react-router';
+
 export const NotFoundPage = () => {
+    const navigate = useNavigate();
     return (
-        <div>404</div>
+        <Flex vertical align='center' justify='center'>
+            <img className="image" src={image} alt="" />
+            <Typography.Paragraph type='secondary' >Whoops! This page must be a ghost - it's not here!</Typography.Paragraph>
+            <Flex justify='center'><Button variant='solid' color='primary' onClick={() => navigate('/')}>Find shelter</Button></Flex>
+        </Flex>
     )
 }
 
