e99c65b04ed6cfcf50c2f2570c05ce880153d758
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2026-01-21 17:25:48 +0300

    added banners

--- src/pages/SearchResultsPage/SearchResultsPage.tsx
+++ src/pages/SearchResultsPage/SearchResultsPage.tsx
@@ -1,12 +1,16 @@
-import { Flex } from "antd";
+import { Flex, Typography } from "antd";
 import { useCallback, useContext, useMemo } from "react"
 import cn from 'classnames'
 import style from './SearchResultsPage.module.scss'
-import { TicketForm } from "src/widgets";
+import { Banner, TicketForm } from "src/widgets";
 import { useSearchParams } from "react-router";
 import { StationsContext } from "src/contexts/StationsContext";
 import { parseDate } from "./helpers";
 import { PageLayout } from "src/layouts";
+import banner1 from '@images/banner1.png'
+import banner2 from '@images/banner2.png'
+
+const {Title, Paragraph} = Typography
 
 export const SearchResultsPage = () => {
 
@@ -25,9 +29,14 @@ export const SearchResultsPage = () => {
 
     return (
         <PageLayout>
-            <h1>Search results</h1>
+            <Title level={1}>Search results</Title>
             <TicketForm initialValues={initialValues} handleFinish={handleFinish} stationsIsLoading={loading}
             formType="result" stationList={stationList ?? []} />
+            <Flex vertical gap={34}>
+            <Banner text='Planning your holidays' image={banner1}/>
+            <Banner text='Train tourism packages' image={banner2}/>
+            <Paragraph>Our trains don't just transport people, they transport emotions and stories! From the mountains of Darjeeling to the beaches of Goa, we connect more than just stations. As Raj Koothrappali would say, "In India, we don't just ride trains, we experience cosmic journeys with occasional cow delays." Book now and embrace the colorful chaos!</Paragraph>
+            </Flex>
         </PageLayout>
     )
 }
