be9431a83c0325564f2066a658f53495baaf6bb3
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2026-02-20 15:05:34 +0300

    changed configs for gh pages

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,35 +2,40 @@ import { createBrowserRouter } from 'react-router';
 import { HomePage, NotFoundPage, PaymentPage, ReviewBookingPage, SearchResultsPage, SuccessPage } from '../pages';
 import { MainLayout } from 'src/layouts/MainLayout/MainLayout';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: '/search-results',
-        element: <SearchResultsPage />,
-      },
-      {
-        path: '/review-booking',
-        element: <ReviewBookingPage />,
-      },
-      {
-        path: '/payment',
-        element: <PaymentPage />,
-      },
-      {
-        path: '/success',
-        element: <SuccessPage />,
-      },
-      {
-        path: '*',
-        element: <NotFoundPage />,
-      },
-    ],
-  },
-]);
+const basename = import.meta.env.BASE_URL;
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: '/search-results',
+          element: <SearchResultsPage />,
+        },
+        {
+          path: '/review-booking',
+          element: <ReviewBookingPage />,
+        },
+        {
+          path: '/payment',
+          element: <PaymentPage />,
+        },
+        {
+          path: '/success',
+          element: <SuccessPage />,
+        },
+        {
+          path: '*',
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ],
+  { basename: basename },
+);
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => ({
   plugins: [react()],
   resolve: {
     alias: {
@@ -11,4 +11,5 @@ export default defineConfig({
       'src': path.resolve(__dirname, './src'),
     },
   },
-})
+  base: mode === 'production' ? '/javarush.3.react' : '/'
+}))
